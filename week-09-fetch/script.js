7ca029bfb07bf04f6df45cbbb50f4411dcccc08a
Max Henry <98223715+MaxAttack7@users.noreply.github.com>
2022-03-31 19:52:25 -0400

    Update script.js

--- week-09-fetch/script.js
+++ week-09-fetch/script.js
@@ -1,14 +1,11 @@
-// Loads content for a Wikipedia article and displays it in the page.
 async function loadContent() {
-  const pageId = 'Cat';
+  const pageId = 'Millersville_University_of_Pennsylvania';
   const response = await fetch(
     'https://en.wikipedia.org/w/api.php?' +
     'action=parse&formatversion=2&format=json&origin=*&page='
     + pageId);
   const json = await response.json();
   const article = json.parse;
-
-  // Get the links in the Wikipedia article, and display them in the page.
   const linksElement = document.getElementById('links');
   for (const link of article.links) {
     const linkElement = document.createElement('a');
@@ -22,33 +19,54 @@ async function loadContent() {
   }
 
   loadImages(article.images);
+  loadLanguages(article.langlinks)
+  loadExternal(article.externallinks)
+
 }
 
-// Takes an array of image file names, uses the Wikipedia API to get the full
-// URL for each one, and then displays them in the page.
+
 async function loadImages(images) {
   const imagesContainer = document.getElementById('images');
   for (const image of images) {
-    // image is a local filename (e.g. Cat.jpg), so call the Wikipedia API to
-    // get the full URL for the image.
     const response = await fetch(
       'https://en.wikipedia.org/w/api.php'
       + '?action=query&prop=imageinfo&iiprop=url&format=json&formatversion=2&origin=*'
       + '&titles=Image:' + image );
       const json = await response.json();
-
       const imageUrl = json.query.pages[0].imageinfo[0].url;
 
-      // Skip images that can't be displayed as img elements
       if (imageUrl.endsWith('.ogg')
           || imageUrl.endsWith('.tiff')
           || imageUrl.endsWith('.webm')
           || imageUrl.endsWith('.ogv')) {
         continue;
       }
-
       const imageElement = document.createElement('img');
       imageElement.src = imageUrl;
       imagesContainer.appendChild(imageElement);
   }
 }
+
+async function loadLanguages(langLinks){
+  const linksElement = document.getElementById('languages');
+    for (const lang of langLinks) {
+      const linkElement = document.createElement('p');
+      linkElement.innerText = lang.langname;
+      const liElement = document.createElement('li');
+      liElement.appendChild(linkElement);
+      linksElement.appendChild(liElement);
+
+      }
+    }
+async function loadExternal(externallinks){
+  const linksElement = document.getElementById('externallinks');
+    for (const links of externallinks) {
+      const linkElement = document.createElement('p');
+      linkElement.href = 'https://en.wikipedia.org/wiki/' + externallinks;
+      linkElement.innerText = externallinks;
+      const liElement = document.createElement('li');
+      liElement.appendChild(linkElement);
+      linksElement.appendChild(liElement);
+      break;
+        }
+    }
